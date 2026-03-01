40b7856604b4209936ef0881cca9ad1923b5fff0
Yago Gonzalez <jagogc@gmail.com>
2026-03-01 01:26:30 +0000

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,6 +23,14 @@ export default defineConfig({
       {
         extends: true,
         plugins: [storybookTest({ configDir: "./packages/core/.storybook" })],
+        optimizeDeps: {
+          include: [
+            "@storybook/addon-vitest/internal/setup-file",
+            "@storybook/addon-vitest/internal/global-setup",
+            "@storybook/addon-vitest/internal/test-utils",
+            "react-dom/test-utils",
+          ],
+        },
         test: {
           name: "storybook",
           browser: {
