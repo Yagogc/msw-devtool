51a22de30b67e112992f0f162cf1fb8799307805
Yago Gonzalez <jagogc@gmail.com>
2026-04-13 00:39:16 +0100

    Fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
     baseURL,
     extraHTTPHeaders: process.env.VERCEL_AUTOMATION_BYPASS_SECRET
       ? {
-          "x-vercel-protection-bypass":
-            process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
+          "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
         }
       : {},
     trace: "on-first-retry",
